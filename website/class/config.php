d5e808d3613fb0b20d36ed110df551b55a6a6bce
Nereziel <nerezskill@gmail.com>
2023-11-07 18:12:53 +0100

    forgot remove testing cfg

--- website/class/config.php
+++ website/class/config.php
@@ -1,10 +1,10 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'cs2server');
-define('DB_USER', 'cs2server');
-define('DB_PASS', 'SupRCupRH35l0p1c0+*');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 
-define('STEAM_API_KEY', '37D90C2FFAC89E178B0D53DB42A4981C');
+define('STEAM_API_KEY', '');
 define('STEAM_DOMAIN_NAME', '');
 define('STEAM_LOGOUT_PAGE', '');
 define('STEAM_LOGIN_PAGE', '');
