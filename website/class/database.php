3f8e5db2ae69f315df0e8fefc0cf0b0ed7423e00
Nereziel <nerezskill@gmail.com>
2023-11-14 13:12:26 +0100

    add support for custom port in database

--- website/class/database.php
+++ website/class/database.php
@@ -4,7 +4,7 @@ class DataBase {
     private $PDO;
 
     public function __construct() {
-        $this->PDO = new PDO("mysql:host=".DB_HOST."; dbname=".DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        $this->PDO = new PDO("mysql:host=".DB_HOST."; port=".DB_PORT."; dbname=".DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     }
     public function select($query, $bindings = []) {
         $STH = $this->PDO->prepare($query);
