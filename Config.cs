9f982d1e63e9298c67a262d0e038649d3b6c0373
Dawid Bepierszcz <41084667+daffyyyy@users.noreply.github.com>
2023-11-15 13:03:16 +0100

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -26,19 +26,19 @@ namespace WeaponPaints
         public override int Version { get; set; } = 2;
 
         [JsonPropertyName("DatabaseHost")]
-        public string DatabaseHost { get; set; } = "localhost";
+        public string DatabaseHost { get; set; } = "";
 
         [JsonPropertyName("DatabasePort")]
         public int DatabasePort { get; set; } = 3306;
 
         [JsonPropertyName("DatabaseUser")]
-        public string DatabaseUser { get; set; } = "DBUSER";
+        public string DatabaseUser { get; set; } = "";
 
         [JsonPropertyName("DatabasePassword")]
-        public string DatabasePassword { get; set; } = "DBPASSWORD";
+        public string DatabasePassword { get; set; } = "";
 
         [JsonPropertyName("DatabaseName")]
-        public string DatabaseName { get; set; } = "DBDATABASE";
+        public string DatabaseName { get; set; } = "";
 
         [JsonPropertyName("CmdRefreshCooldownSeconds")]
         public int CmdRefreshCooldownSeconds { get; set; } = 60;
