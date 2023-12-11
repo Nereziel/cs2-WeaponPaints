928c1e1466aa43e23e97b46f5c582bb055321f8f
Nilsu Derinder <44870116+exababy@users.noreply.github.com>
2023-12-11 18:46:34 +0300

    bootstrap update

--- website/index.php
+++ website/index.php
@@ -48,6 +48,8 @@ if (isset($_SESSION['steamid'])) {
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
 		integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
 		crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="style.css">
 	<title>CS2 Simple Weapon Paints</title>
 </head>
