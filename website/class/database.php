8f0e8a0a63efd7d2c77a75a4a9b4c699f3a6143b
Nereziel <nerezskill@gmail.com>
2024-02-03 16:43:50 +0100

    forgot file and maybe min compatible php 5.5

--- website/class/database.php
+++ website/class/database.php
@@ -17,11 +17,12 @@ class DataBase {
         try {
             // Establish a connection to the database using PDO
             $this->PDO = new PDO(
-                "mysql:host=".DB_HOST."; port=".DB_PORT."; dbname=".DB_NAME, 
+                "mysql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME, 
                 DB_USER, 
-                DB_PASS, 
-                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+                DB_PASS
             );
+            // Set the connection to use utf8 encoding
+            $this->PDO->exec("SET NAMES utf8");
         }
         catch(PDOException $ex) {
             // Display error message if connection fails
@@ -37,14 +38,16 @@ class DataBase {
      * @param array $bindings An associative array of parameters and their values.
      * @return array|false Returns an array of rows as associative arrays or false if no results are found.
      */
-    public function select($query, $bindings = []) {
+    public function select($query, $bindings = array()) {
         // Prepare and execute the SQL query
         $STH = $this->PDO->prepare($query);
         $STH->execute($bindings);
 
         // Fetch the results as associative arrays
         $result = $STH->fetchAll(PDO::FETCH_ASSOC);
-        $result ??= false; // Set $result to false if it's null
+        if ($result === false) {
+            $result = array(); // Set $result to an empty array if no results found
+        }
         return $result;
     }
 
@@ -55,7 +58,7 @@ class DataBase {
      * @param array $bindings An associative array of parameters and their values.
      * @return bool Returns true on success or false on failure.
      */
-    public function query($query, $bindings = []) {
+    public function query($query, $bindings = array()) {
         // Prepare and execute the SQL query
         $STH = $this->PDO->prepare($query);
         return $STH->execute($bindings);
--- website/class/utils.php
+++ website/class/utils.php
@@ -11,20 +11,20 @@ class UtilsClass
      *
      * @return array An associative array containing skin data.
      */
-    public static function skinsFromJson(): array
+    public static function skinsFromJson()
     {
-        $skins = [];
+        $skins = array();
         $jsonFilePath = __DIR__ . "/../data/skins.json";
 
         if (file_exists($jsonFilePath) && is_readable($jsonFilePath)) {
             $json = json_decode(file_get_contents($jsonFilePath), true);
 
             foreach ($json as $skin) {
-                $skins[(int) $skin['weapon_defindex']][(int) $skin['paint']] = [
+                $skins[(int) $skin['weapon_defindex']][(int) $skin['paint']] = array(
                     'weapon_name' => $skin['weapon_name'],
                     'paint_name' => $skin['paint_name'],
                     'image_url' => $skin['image'],
-                ];
+                );
             }
         } else {
             // Handle file not found or unreadable error
@@ -39,17 +39,17 @@ class UtilsClass
      *
      * @return array An associative array containing weapon data.
      */
-    public static function getWeaponsFromArray(): array
+    public static function getWeaponsFromArray()
     {
-        $weapons = [];
+        $weapons = array();
         $skinsData = self::skinsFromJson();
 
         foreach ($skinsData as $key => $value) {
-            $weapons[$key] = [
+            $weapons[$key] = array(
                 'weapon_name' => $value[0]['weapon_name'],
                 'paint_name' => $value[0]['paint_name'],
                 'image_url' => $value[0]['image_url'],
-            ];
+            );
         }
 
         return $weapons;
@@ -60,32 +60,32 @@ class UtilsClass
      *
      * @return array An associative array containing knife types data.
      */
-    public static function getKnifeTypes(): array
+    public static function getKnifeTypes()
     {
-        $knifes = [];
+        $knifes = array();
         $weaponsData = self::getWeaponsFromArray();
 
-        $allowedKnifeKeys = [
+        $allowedKnifeKeys = array(
             500, 503, 505, 506, 507, 508, 509, 512, 514, 515,
             516, 517, 518, 519, 520, 521, 522, 523, 525
-        ];
+        );
 
         foreach ($weaponsData as $key => $weapon) {
             if (in_array($key, $allowedKnifeKeys)) {
-                $knifes[$key] = [
+                $knifes[$key] = array(
                     'weapon_name' => $weapon['weapon_name'],
                     'paint_name' => rtrim(explode("|", $weapon['paint_name'])[0]),
                     'image_url' => $weapon['image_url'],
-                ];
+                );
             }
         }
 
         // Add default knife
-        $knifes[0] = [
+        $knifes[0] = array(
             'weapon_name' => "weapon_knife",
             'paint_name' => "Default knife",
             'image_url' => "https://raw.githubusercontent.com/Nereziel/cs2-WeaponPaints/main/website/img/skins/weapon_knife.png",
-        ];
+        );
 
         ksort($knifes);
         return $knifes;
@@ -97,19 +97,18 @@ class UtilsClass
      * @param array $temp An array containing the selected skins data.
      * @return array An associative array containing selected skins data.
      */
-    public static function getSelectedSkins(array $temp): array
+    public static function getSelectedSkins($temp)
     {
-        $selected = [];
+        $selected = array();
 
         foreach ($temp as $weapon) {
-            $selected[$weapon['weapon']] =  [
+            $selected[$weapon['weapon']] =  array(
                 'weapon_paint_id' => $weapon['paint'],
                 'weapon_seed' => $weapon['seed'],
                 'weapon_wear' => $weapon['wear'],
-            ];
+            );
         }
 
         return $selected;
     }
 }
-
