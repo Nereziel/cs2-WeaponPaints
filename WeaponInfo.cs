e209b12829ac44c264e06da90a4bb409c6c6c528
StefanX <60297289+stefanx111@users.noreply.github.com>
2024-10-18 19:54:21 +0300

    mistake

--- WeaponInfo.cs
+++ WeaponInfo.cs
@@ -1,29 +1,32 @@
-﻿public class WeaponInfo
+﻿namespace WeaponPaints
 {
-	public int Paint { get; set; }
-	public int Seed { get; set; } = 0;
-	public float Wear { get; set; } = 0f;
-	public string Nametag { get; set; } = "";
-	public KeyChainInfo? KeyChain { get; set; }
-	public List<StickerInfo> Stickers { get; set; } = new List<StickerInfo>();
-}
+	public class WeaponInfo
+	{
+		public int Paint { get; set; }
+		public int Seed { get; set; } = 0;
+		public float Wear { get; set; } = 0f;
+		public string Nametag { get; set; } = "";
+		public KeyChainInfo? KeyChain { get; set; }
+		public List<StickerInfo> Stickers { get; set; } = new List<StickerInfo>();
+	}
 
-public class StickerInfo
-{
-	public uint Id { get; set; }
-	public uint Schema { get; set; }
-	public float OffsetX { get; set; }
-	public float OffsetY { get; set; }
-	public float Wear { get; set; }
-	public float Scale { get; set; }
-	public float Rotation { get; set; }
-}
+	public class StickerInfo
+	{
+		public uint Id { get; set; }
+		public uint Schema { get; set; }
+		public float OffsetX { get; set; }
+		public float OffsetY { get; set; }
+		public float Wear { get; set; }
+		public float Scale { get; set; }
+		public float Rotation { get; set; }
+	}
 
-public class KeyChainInfo
-{
-	public uint Id { get; set; }
-	public float OffsetX { get; set; }
-	public float OffsetY { get; set; }
-	public float OffsetZ { get; set; }
-	public uint Seed { get; set; }
+	public class KeyChainInfo
+	{
+		public uint Id { get; set; }
+		public float OffsetX { get; set; }
+		public float OffsetY { get; set; }
+		public float OffsetZ { get; set; }
+		public uint Seed { get; set; }
+	}
 }
